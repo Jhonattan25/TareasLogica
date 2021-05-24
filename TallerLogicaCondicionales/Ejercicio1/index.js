326bf52a785e06423e049b185086a74aa0383391
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-05-24 10:59:46 -0500

    Se modifico el nombre de algunas variables en los ejercicios de condicionales

--- TallerLogicaCondicionales/Ejercicio1/index.js
+++ TallerLogicaCondicionales/Ejercicio1/index.js
@@ -2,9 +2,9 @@
    1. Cree un programa que lea la edad de un usuario e imprima un mensaje que diga si el usuario es mayor de edad o no.
 */
 
-let edad = parseInt(prompt("Ingrese la edad"));
+let age = parseInt(prompt("Ingrese la edad"));
 
-if(edad >= 18){
+if(age >= 18){
     console.log("Es mayor de edad");
 }else{
     console.log("No es mayor de edad");
--- TallerLogicaCondicionales/Ejercicio2/index.js
+++ TallerLogicaCondicionales/Ejercicio2/index.js
@@ -2,11 +2,11 @@
   2. En un supermercado se tiene los siguientes productos: lentejas, crema, arroz y vino. Las lentejas y el arroz no pagan IVA, el vino y la crema si. Cree un programa que reciba el nombre de alguno de los productos mencionados y muestre si el producto paga IVA o no.
 */
 
-let nombreProducto = (prompt("Ingrese nombre del producto"));
+let productName = (prompt("Ingrese el nombre del producto"));
 
-if(nombreProducto === "lentejas" || nombreProducto === "arroz"){
+if(productName === "lentejas" || productName === "arroz"){
     console.log("No paga IVA");
 }
-if(nombreProducto === "vino" && nombreProducto === "crema"){
+if(productName === "vino" && productName === "crema"){
     console.log("Debe pagar IVA");
 }
